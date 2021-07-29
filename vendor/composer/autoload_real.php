f51de3bbee591e8b733d8980aeffb1b34e3b59f2
DZ8540 <d.z.2021@inbox.ru>
2021-07-29 13:54:29 +0300

    Remove node_modules directory and add .gitignore file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitf06e50aed95c7dfa3c2326fd76391eda
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf06e50aed95c7dfa3c2326fd76391eda', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf06e50aed95c7dfa3c2326fd76391eda', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf06e50aed95c7dfa3c2326fd76391eda::getInitializer($loader));
         } else {
