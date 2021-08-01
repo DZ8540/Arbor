1a0759db1c755bdea7a62b32f3f7feb3084b9979
8DZ8 <d.z.2021@inbox.ru>
2021-08-01 14:15:59 +0300

    Change controller style for laravel style

--- app/Http/Controllers/User/MainController.php
+++ app/Http/Controllers/User/MainController.php
@@ -7,43 +7,53 @@ use Illuminate\Http\Request;
 
 class MainController extends BaseController
 {
-	public function index() {
+	public function index()
+  {
 		return view('User.index');
 	}
 
-	public function about() {
+	public function about()
+  {
 		return view('User.about');
 	}
 
-	public function services() {
+	public function services()
+  {
 		return view('User.services');
 	}
 
-	public function news() {
+	public function news()
+  {
 		return view('User.news');
 	}
 
-	public function news_item($slug = null) {
+	public function news_item($slug = null)
+  {
 		return view('User.news_item', compact('slug'));
 	}
 
-	public function catalog() {
+	public function catalog()
+  {
 		return view('User.catalog');
 	}
 
-	public function category($slug = null) {
+	public function category($slug = null)
+  {
 		return view('User.category', compact('slug'));
 	}
 
-	public function product($category_slug = null, $product_slug = null) {
+	public function product($category_slug = null, $product_slug = null)
+  {
 		return view('User.product', compact('category_slug', 'product_slug'));
 	}
 
-	public function cart() {
+	public function cart()
+  {
 		return view('User.cart');
 	}
 
-	public function order() {
+	public function order()
+  {
 		return view('User.order');
 	}
 }
